035ae6d407bdae2852443cef93676b1c9716be22
alaxhenry <alaxhenry0121@gmail.com>
2022-12-16 17:38:45 +0900

    Initial Commit

---
+++ BookAppAnimation/BookAppAnimationApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookAppAnimationApp.swift
+//  BookAppAnimation
+//
+//  Created by Seungchul Ha on 2022/12/16.
+//
+
+import SwiftUI
+
+@main
+struct BookAppAnimationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
